d30595d35f1c3cddd539073c39f19e595d6dd523
Julius Garrido <jflgarrido@gmail.com>
2021-12-17 16:17:15 -0700

    commit tree 3abbea56e7e7f337e6cc27e99cef724932cf9dce

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+ string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, "")
+  }
+  static titleize(string){
+    let splitString = string.split(" ")
+    let forbiddenWordsArray = ["the","a","an","but","of","and","for","at","by","from"]
+    
+    let emptyArray = []
+    for(const item of splitString){
+        if(forbiddenWordsArray.includes(item)){
+          emptyArray.push(item)
+         }
+        else {
+          emptyArray.push(this.capitalize(item))
+        } 
+    }
+    emptyArray.unshift(this.capitalize(emptyArray.shift()))
+    return emptyArray.join(" ") 
+  }
+   
 }
